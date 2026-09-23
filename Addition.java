b35ef8b5a5e745c1d07f8e1bd5cd1076b1d19c2b
japmeet123 <japmeet060@gmail.com>
2026-09-23 09:52:04 -0400

    Create Addition.java

---
+++ Addition.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        String firstName = "Japmeet";
+        String lastName = "Chahal";
+        String fullName = firstName + " " + lastName;
+        System.out.println()
+    }
